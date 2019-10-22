db6338df61752641c0ac1cdf3781f76767252ec9
Marcello TI <marcello88costa@yahoo.com.br>
2019-10-21 21:11:16 -0300

    Doing the module GoBarber

--- developer/code/Bootcamp/gobarber/src/app/controllers/AppointmentController.js
+++ developer/code/Bootcamp/gobarber/src/app/controllers/AppointmentController.js
@@ -6,6 +6,9 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
+
 class AppointmentController {
   async index(req, res) {
     const { page = 1 } = req.query;
@@ -120,11 +123,22 @@ class AppointmentController {
   }
 
   async delete(req, res) {
-    const appointment = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
     if (appointment.user_id !== req.userId) {
-      console.log(appointment.user_id);
-      console.log(req.userId);
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment",
       });
@@ -138,9 +152,18 @@ class AppointmentController {
       });
     }
 
-    appointment.canceled_at = new Date();
+    // Converting date/time now to correct format
+    const dateNow = format(new Date(), "yyyy-MM-dd'T'HH:mm:ss'.'SSS'Z'", {
+      locale: pt,
+    });
+
+    appointment.canceled_at = dateNow;
 
     await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment,
+    });
 
     return res.json(appointment);
   }
---
+++ developer/code/Bootcamp/gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1b464408715ec8',
+    pass: '5fa08b4035787f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
