4afe254a81777367712ea87634671229395dddfd
Marcello TI <marcello88costa@yahoo.com.br>
2019-10-28 20:38:37 -0300

    Challenge 3 completed

--- developer/code/Bootcamp/gympoint/src/database/index.js
+++ developer/code/Bootcamp/gympoint/src/database/index.js
@@ -5,10 +5,11 @@ import Student from '../app/models/Student';
 import Plan from '../app/models/Plan';
 import Enrolment from '../app/models/Enrolment';
 import Checkin from '../app/models/Checkin';
+import HelpOrder from '../app/models/HelpOrder';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Student, Plan, Enrolment, Checkin];
+const models = [User, Student, Plan, Enrolment, Checkin, HelpOrder];
 
 class Database {
   constructor() {
--- developer/code/Bootcamp/gympoint/src/routes.js
+++ developer/code/Bootcamp/gympoint/src/routes.js
@@ -7,6 +7,7 @@ import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
 import EnrolmentController from './app/controllers/EnrolmentController';
 import CheckinController from './app/controllers/CheckinController';
+import HelpOrderController from './app/controllers/HelpOrderController';
 
 // Authentication Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -25,9 +26,16 @@ routes.post('/sessions', SessionController.store);
 routes.get('/students/:id/checkins', CheckinController.show);
 routes.post('/students/:id/checkins', CheckinController.store);
 
+// HelpOrder
+routes.post('/students/:id/help-orders', HelpOrderController.store);
+routes.get('/students/:id/help-orders', HelpOrderController.show);
+
 // With this “use” statement the routes below will use middleware.
 // The above routes will remain unused middleware
 routes.use(authMiddleware);
+
+routes.get('/students/help-orders', HelpOrderController.index);
+routes.put('/help-orders/:id/answer', HelpOrderController.update);
 
 // Student Routes
 routes.get('/students', StudentController.index);
