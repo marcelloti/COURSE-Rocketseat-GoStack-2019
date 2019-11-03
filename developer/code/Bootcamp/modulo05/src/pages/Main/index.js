3aa6e34c19199d8973a3455f236d44715178b084
Marcello TI <marcello88costa@yahoo.com.br>
2019-11-03 19:12:23 -0300

    Challenge 5 completed

---
+++ developer/code/Bootcamp/modulo05/src/pages/Main/index.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
+import api from '../../services/api';
+
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+    searchError: false,
+  };
+
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados do localStorage
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      newRepo: e.target.value,
+    });
+  };
+
+  handleSubmit = async e => {
+    try {
+      e.preventDefault();
+
+      this.setState({ loading: true });
+
+      const { newRepo, repositories } = this.state;
+
+      const repoExists = repositories.find(
+        repository => repository.name === newRepo
+      );
+      if (repoExists) {
+        throw new Error('Repositório duplicado');
+      }
+
+      const response = await api.get(`/repos/${newRepo}`);
+
+      const data = {
+        name: response.data.full_name,
+      };
+
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+        searchError: false,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        searchError: true,
+      });
+    }
+  };
+
+  render() {
+    const { newRepo, repositories, loading, searchError } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit} searchError={searchError}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
